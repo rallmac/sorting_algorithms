79e44c266e63d666f7927c415e0b7813648f6e87
rallmac2 <root@tobijahdevice>
2024-04-28 18:33:33 +0100

    Bubble sort task 1

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -1,3 +1,5 @@
+#include <stdlib.h>
+#include <stdio.h>
 #include "sort.h"
 
 /**
@@ -8,28 +10,19 @@
 */
 void bubble_sort(int *array, size_t size)
 {
-    int temp;
-    size_t i, j, k;
-    
-    for (i = 0; i < size -1; i++)
-    {
-        for (j = 0; j < size - i - 1; j++)
-        {
-            if (array[j] > array[j + 1])
-            {
-                temp = array[j];
-                array[j] = array[j + 1];
-                array[j + 1] = temp;
-            }
-        }
-        for (k = 0; k < size; k++)
-        {
-            printf("%d", array[k]);
-            if (k < size - 1)
-            {
-                printf(", ");
-            }
-        }
-        printf("\n");
-    }
+	int temp;
+	size_t i, j;
+
+	for (i = 0; i < size - 1; i++)
+	{
+		for (j = 0; j < size - i - 1; j++)
+		{
+			if (array[j] > array[j + 1])
+			{
+				temp = array[j];
+				array[j] = array[j + 1];
+				array[j + 1] = temp;
+			}
+		}
+	}
 }
--- sort.h
+++ sort.h
@@ -21,7 +21,7 @@ typedef struct listint_s
 } listint_t;
 
 int _putchar(char c);
-void print_array(int array[], int n);
+void print_array(int *array, size_t size);
 void bubble_sort(int *array, size_t size);
 void insertion_sort_list(listint_t **list);
 void selection_sort(int *array, size_t size);
