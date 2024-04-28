e68262df49a5a13e101b6f9991d896b0969486af
rallmac2 <root@tobijahdevice>
2024-04-28 18:57:03 +0100

    Bubble sort task 1 3rd try

--- sort.h
+++ sort.h
@@ -7,7 +7,7 @@
 
 /**
  * struct listint_s - Doubly linked list node
- * 
+ *
  * @n: Integer sorted in the node
  * @prev: Pointer to the previous element of the list
  * @next: Pointer to the next element of the list
@@ -15,9 +15,9 @@
 
 typedef struct listint_s
 {
-    const int n;
-    struct listint_s *prev;
-    struct listint_s *next;
+	const int n;
+	struct listint_s *prev;
+	struct listint_s *next;
 } listint_t;
 
 int _putchar(char c);
